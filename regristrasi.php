a24b823f87748c6660d715916eff30c73f59fa23
ERNISA YUNIARTI SATRIYO PUTRI <43512652+ernisayuniarti@users.noreply.github.com>
2018-10-05 10:22:32 +0700

    Update regristrasi.php
    ernisa yuniarti
    6701174038

--- regristrasi.php
+++ regristrasi.php
@@ -1,11 +1,24 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Form Registrasi</title>
-</head>
-<body>
-
-
-
-</body>
-</html>
+<form action="prosesregistrasi.php" method="POST">
+	<table>
+		<tr>
+			<td>NIM</td>
+			<td>:</td>
+			<td><input type="text" name="nim"></td>
+		</tr>
+		<tr>
+			<td>NAMA</td>
+			<td>:</td>
+			<td><input type="text" name="nama"></td>
+		</tr>
+		<tr>
+			<td>EMAIL</td>
+			<td>:</td>
+			<td><input type="email" name="email"></td>
+		</tr>
+		<tr>
+		<td></td>
+		<td></td>
+		<td><input type="submit" name="submit" value="Registrasi"></td>
+		</tr>
+	</table>
+</form>
