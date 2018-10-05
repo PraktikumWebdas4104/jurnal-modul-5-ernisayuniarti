db2fdd31e031ace14523a2d4ae0eacd74674f0a6
ERNISA YUNIARTI SATRIYO PUTRI <43512652+ernisayuniarti@users.noreply.github.com>
2018-10-05 10:21:30 +0700

    Update koneksi.php
    ernisa yuniarti
    6701174038

--- koneksi.php
+++ koneksi.php
@@ -1,3 +1,28 @@
 <?php
+$koneksi = new mysqli("localhost", "root", "", "mahasiswa_db");
 
+	if (isset($_POST['submit'])) {
+
+		if ($koneksi) {
+			$nim = $_POST['nim'];
+			$nama = $_POST['nama'];
+			$email = $_POST['email'];
+			$status = true;
+
+			if(!is_int($nim) && strlen($nim)!=10 && strlen($nama)!=25 ){
+				echo "registrasi gagal";
+				$status =  false;
+			}
+
+			if ($status){
+				$sql = $koneksi->query("
+				INSERT INTO `mahasiswa` (`nim`, `nama`, `email`)
+					VALUES (".$nim.", ".$nama.", ".$email.")");
+					echo "REGISTRASI SUKSES"."<br>";
+			}else{
+			}
+		}else{
+			echo "KONEKSI GAGAL";
+		}
+	}
 ?>
